e743c04bfcdfe4030b65ba144709febd0de558c1
n2vqg7x6ky-web <chasepablo310@gmail.com>
2026-03-12 16:26:40 +1100

    Add MapManager.lua

---
+++ RobloxFightingGame/Scripts/MapManager.lua
@@ -0,0 +1,10 @@
+-- Map Manager Script
+-- Handles the map logic and transitions between game areas.
+
+local MapManager = {}
+
+function MapManager:LoadMap(mapName)
+    -- Load the specified map
+end
+
+return MapManager
